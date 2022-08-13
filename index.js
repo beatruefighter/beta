955f165812fd1e75d03bbcd54fd918dc0eed54d9
beatruefighter <104251748+beatruefighter@users.noreply.github.com>
2022-08-13 17:52:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,171 @@
+function edit(){
+ 	      var inputData =  document.getElementById("inputData");
+         var dobData =  document.getElementById("dobData");
+
+     	var genderData = document.getElementById("genderData");
+        var ageData = document.getElementById("ageData");
+        var eduData = document.getElementById("eduData");
+        var lineData = document.getElementById("lineData");
+        var conData = document.getElementById("conData");
+        var colData = document.getElementById("colData");
+        var citData = document.getElementById("citData");
+        var disData = document.getElementById("disData");
+        var pinData = document.getElementById("pinData");
+        var staData = document.getElementById("staData");
+
+
+
+
+
+        
+
+      var updateData = document.getElementById("updateData");
+      var editData = document.getElementById("editData");
+  
+    inputData.style.display =  "block";
+    dobbData.style.display =  "block";
+
+    genData.style.display =  "block";
+    aageData.style.display =  "block";
+    eeduData.style.display =  "block";
+    lineeData.style.display =  "block";
+
+    connData.style.display =  "block";
+    collData.style.display =  "block";
+    cittData.style.display =  "block";
+    distData.style.display =  "block";
+    pinnData.style.display =  "block";
+    statData.style.display =  "block";
+
+    
+    
+
+
+
+
+    
+
+
+    editData.style.display = "none";
+    updateData.style.display = "block";
+  
+
+}
+
+function update(){
+	  var inputData = document.getElementById("inputData");
+    var dobData = document.getElementById("dobData");
+
+  	var genderData = document.getElementById("genderData");
+    var ageData = document.getElementById("ageData");
+    var eduData = document.getElementById("eduData");
+    var lineData = document.getElementById("lineData");
+    var conData = document.getElementById("conData");
+    var colData = document.getElementById("colData");
+    var citData = document.getElementById("citData");
+    var disData = document.getElementById("disData");
+    var pinData = document.getElementById("pinData");
+    var staData = document.getElementById("staData");
+
+
+
+
+
+    
+
+
+  
+ 
+    var updateData = document.getElementById("updateData");
+  var editData = document.getElementById("editData");
+  var textData = document.getElementById("textData");
+  var dobData = document.getElementById("dobData");
+
+  var genderData = document.getElementById("genderData");
+  var ageData = document.getElementById("ageData");
+  var eduData = document.getElementById("eduData");
+  var lineData = document.getElementById("lineData");
+  var conData = document.getElementById("conData");
+  var colData = document.getElementById("colData");
+  var citData = document.getElementById("citData");
+  var disData = document.getElementById("disData");
+  var pinData = document.getElementById("pinData");
+  var staData = document.getElementById("staData");
+
+
+
+
+
+
+
+  
+  var inputDataValue = document.getElementById("inputData").value;
+  var dobbDataValue = document.getElementById("dobbData").value;
+
+  var genDataValue = document.getElementById("genData").value;
+  var aageDataValue = document.getElementById("aageData").value;
+  var eeduDataValue = document.getElementById("eeduData").value;
+  var lineeDataValue = document.getElementById("lineeData").value;
+  var connDataValue = document.getElementById("connData").value;
+  var collDataValue = document.getElementById("collData").value;
+  var cittDataValue = document.getElementById("cittData").value;
+  var distDataValue = document.getElementById("distData").value;
+  var pinnDataValue = document.getElementById("pinnData").value;
+  var statDataValue = document.getElementById("statData").value;
+
+
+
+
+
+  
+
+
+  
+   textData.innerHTML = inputDataValue;
+   dobData.innerHTML = dobbDataValue;
+
+   genderData.innerHTML = genDataValue;
+   ageData.innerHTML = aageDataValue;
+   eduData.innerHTML = eeduDataValue;
+   lineData.innerHTML = lineeDataValue;
+   conData.innerHTML = connDataValue;
+   colData.innerHTML = collDataValue;
+   citData.innerHTML = cittDataValue;
+   disData.innerHTML = distDataValue;
+   pinData.innerHTML = pinnDataValue;
+   staData.innerHTML = statDataValue;
+
+
+
+
+
+   
+
+
+   
+   
+   inputData.style.display = "none";
+   dobbData.style.display = "none";
+
+   genData.style.display = "none";
+   aageData.style.display = "none";
+   eeduData.style.display = "none";
+   lineeData.style.display = "none";
+   connData.style.display="none";
+   collData.style.display="none";
+   cittData.style.display="none";
+   distData.style.display="none";
+   pinnData.style.display="none";
+   statData.style.display="none";
+
+
+
+
+ 
+
+
+  editData.style.display = "block";
+  updateData.style.display = "none";
+  
+
+}
